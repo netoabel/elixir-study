9b4de98194647430b8960f02ad4243d1244638d0
netoabel <abel.neto@gmail.com>
2018-04-21 12:02:24 -0300

    Adds recursive product calculation

---
+++ drop/lib/overall.ex
@@ -0,0 +1,13 @@
+defmodule Overall do
+  def product(list) do
+    product(list, 1)
+  end
+
+  def product([], accumulated_product) do
+    accumulated_product
+  end
+
+  def product([head | tail], accumulated_product) do
+    product(tail, head * accumulated_product)
+  end
+end
